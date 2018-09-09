fb41de9942b3221f66e68124370343a67616894b
Eric X <123MegaApple123@gmail.com>
2018-09-09 00:50:13 -0500

    Add options
    Well, most of them

--- index.js
+++ index.js
@@ -7,7 +7,13 @@
 
   const $btnExample = document.querySelector("[data-button=\"example\"]");
 
-  // const $config = document.querySelector("[data-button=\"config\"]");
+  const $btnConfig = document.querySelector("[data-button=\"config\"]");
+  const $diaConfig = document.querySelector("[data-dialog=\"config\"]");
+  const $btnConfigClose = document.querySelector("[data-button=\"config-close\"]");
+  const $chkConfigCountryTitle = document.querySelector("[data-checkbox=\"country-title\"]");
+  const $chkConfigCountryAlt = document.querySelector("[data-checkbox=\"country-alt\"]");
+  const $chkConfigOutputInput = document.querySelector("[data-checkbox=\"output-input\"]");
+  const $chkConfigIgnoreErrors = document.querySelector("[data-checkbox=\"ignore-errors\"]");
 
   const $btnErrors = document.querySelector("[data-button=\"errors\"]");
   const $diaErrors = document.querySelector("[data-dialog=\"errors\"]");
@@ -19,38 +25,304 @@
   const $input = document.querySelector("#input");
 
   const $output = document.querySelector("#output");
+  const $outputErrors = document.querySelector("#output-errors");
   const $diaOutput = document.querySelector("[data-dialog=\"output\"]");
   const $btnOutputClose = document.querySelector("[data-button=\"output-close\"]");
 
   /* flags list */
-  const FLAGS = Object.freeze([
-    "AD", "AE", "AF", "AG", "AI", "AL", "AM", "AN", "AO", "AR", "AS", "AT", "AU", "AW", "AX", "AZ",
-    "BA", "BB", "BD", "BE", "BF", "BG", "BH", "BI", "BJ", "BM", "BN", "BO", "BR", "BS", "BT", "BV", "BW", "BY", "BZ",
-    "CA", "CC", "CD", "CF", "CG", "CH", "CI", "CK", "CL", "CM", "CN", "CO", "CR", "CS", "CU", "CV", "CX", "CY", "CZ",
-    "DE", "DJ", "DK", "DM", "DO", "DZ",
-    "EC", "EE", "EG", "EH", "ER", "ES", "ET",
-    "FI", "FJ", "FK", "FM", "FO", "FR",
-    "GA", "GB", "GD", "GE", "GF", "GH", "GI", "GL", "GM", "GN", "GP", "GQ", "GR", "GS", "GT", "GU", "GW", "GY",
-    "HK", "HM", "HN", "HR", "HT", "HU",
-    "ID", "IE", "IL", "IN", "IO", "IQ", "IR", "IS", "IT",
-    "JM", "JO", "JP",
-    "KE", "KG", "KH", "KI", "KM", "KN", "KP", "KR", "KW", "KY", "KZ",
-    "LA", "LB", "LC", "LI", "LK", "LR", "LS", "LT", "LU", "LV", "LY",
-    "MA", "MC", "MD", "ME", "MG", "MH", "MK", "ML", "MM", "MN", "MO", "MP", "MQ", "MR", "MS", "MT", "MU", "MV", "MW", "MX", "MY", "MZ",
-    "NA", "NC", "NE", "NF", "NG", "NI", "NL", "NO", "NP", "NR", "NU", "NZ",
-    "OM",
-    "PA", "PE", "PF", "PG", "PH", "PK", "PL", "PM", "PN", "PR", "PS", "PT", "PW", "PY",
-    "QA",
-    "RE", "RO", "RS", "RU", "RW",
-    "SA", "SB", "SC", "SD", "SE", "SG", "SH", "SI", "SJ", "SK", "SL", "SM", "SN", "SO", "SR", "ST", "SV", "SY", "SZ",
-    "TC", "TD", "TF", "TG", "TH", "TJ", "TK", "TL", "TM", "TN", "TO", "TR", "TT", "TV", "TW", "TZ",
-    "UA", "UG", "UM", "US", "UY", "UZ",
-    "VA", "VC", "VE", "VG", "VI", "VN", "VU",
-    "WF", "WS",
-    "YE", "YT",
-    "ZA", "ZM", "ZW",
-    "BRCL", "ITNL",
-  ]);
+  const FLAGS = Object.freeze({
+    "AD": "Andorra",
+    "AE": "United Arab Emirates",
+    "AF": "Afghanistan",
+    "AG": "Antigua and Barbuda",
+    "AI": "Anguilla",
+    "AL": "Albania",
+    "AM": "Armenia",
+    "AN": "Netherlands Antilles",
+    "AO": "Angola",
+    "AR": "Argentina",
+    "AS": "American Samoa",
+    "AT": "Austria",
+    "AU": "Australia",
+    "AW": "Aruba",
+    "AX": "Åland Islands",
+    "AZ": "Azerbaijan",
+
+    "BA": "Bosnia and Herzegovina",
+    "BB": "Barbados",
+    "BD": "Bangladesh",
+    "BE": "Belgium",
+    "BF": "Burkina Faso",
+    "BG": "Bulgaria",
+    "BH": "Bahrain",
+    "BI": "Burundi",
+    "BJ": "Benin",
+    "BL": "Saint Barthélemy",
+    "BM": "Bermuda",
+    "BN": "Brunei Darussalam",
+    // "BO": "Plurinational State of Bolivia",
+    "BO": "Bolivia",
+    // "BQ": "Bonaire, Sint Eustatius and Saba",
+    "BR": "Brazil",
+    "BS": "Bahamas",
+    "BT": "Bhutan",
+    // "BV": "Bouvet Island",
+    "BW": "Botswana",
+    "BY": "Belarus",
+    "BZ": "Belize",
+
+    "CA": "Canada",
+    // "CC": "Cocos (Keeling) Islands",
+    "CD": "Democratic Republic of the Congo",
+    "CF": "Central African Republic",
+    "CG": "Congo",
+    "CH": "Switzerland",
+    "CI": "Côte d'Ivoire",
+    // "CK": "Cook Islands",
+    "CL": "Chile",
+    "CM": "Cameroon",
+    "CN": "China",
+    "CO": "Colombia",
+    "CR": "Costa Rica",
+    "CU": "Cuba",
+    "CV": "Cabo Verde",
+    // "CW": "Curaçao",
+    "CX": "Christmas Island",
+    "CY": "Cyprus",
+    "CZ": "Czechia",
+
+    "DE": "Germany",
+    "DJ": "Djibouti",
+    "DK": "Denmark",
+    "DM": "Dominica",
+    "DO": "Dominican Republic",
+    "DZ": "Algeria",
+
+    "EC": "Ecuador",
+    "EE": "Estonia",
+    "EG": "Egypt",
+    // "EH": "Western Sahara",
+    "ER": "Eritrea",
+    "ES": "Spain",
+    "ET": "Ethiopia",
+    "EU": "European Union",
+
+    "FI": "Finland",
+    "FJ": "Fiji",
+    // "FK": "Falkland Islands (Malvinas)",
+    "FK": "Falkland Islands",
+    "FM": "Federated States of Micronesia",
+    "FO": "Faroe Islands",
+    "FR": "France",
+
+    "GA": "Gabon",
+    "GB": "United Kingdom",
+    "GD": "Grenada",
+    "GE": "Georgia",
+    "GF": "French Guiana",
+    "GG": "Guernsey",
+    "GH": "Ghana",
+    "GI": "Gibraltar",
+    "GL": "Greenland",
+    "GM": "Gambia",
+    "GN": "Guinea",
+    "GP": "Guadeloupe",
+    "GQ": "Equatorial Guinea",
+    "GR": "Greece",
+    // "GS": "South Georgia and the South Sandwich Islands",
+    "GT": "Guatemala",
+    "GU": "Guam",
+    "GW": "Guinea-Bissau",
+    "GY": "Guyana",
+
+    "HK": "Hong Kong",
+    "HM": "Heard Island and McDonald Islands",
+    "HN": "Honduras",
+    "HR": "Croatia",
+    "HT": "Haiti",
+    "HU": "Hungary",
+
+    "ID": "Indonesia",
+    "IE": "Ireland",
+    "IL": "Israel",
+    "IM": "Isle of Man",
+    "IN": "India",
+    // "IO": "British Indian Ocean Territory",
+    "IQ": "Iraq",
+    // "IR": "Islamic Republic of Iran",
+    "IR": "Iran",
+    "IS": "Iceland",
+    "IT": "Italy",
+
+    "JE": "Jersey",
+    "JM": "Jamaica",
+    "JO": "Jordan",
+    "JP": "Japan",
+
+    "KE": "Kenya",
+    "KG": "Kyrgyzstan",
+    "KH": "Cambodia",
+    "KI": "Kiribati",
+    "KM": "Comoros",
+    "KN": "Saint Kitts and Nevis",
+    // "KP": "Democratic People's Republic of Korea",
+    "KP": "North Korea",
+    // "KR": "Republic of Korea",
+    "KR": "South Korea",
+    "KW": "Kuwait",
+    "KY": "Cayman Islands",
+    "KZ": "Kazakhstan",
+
+    // "LA": "Lao People's Democratic Republic",
+    "LA": "Laos",
+    "LB": "Lebanon",
+    "LC": "Saint Lucia",
+    "LI": "Liechtenstein",
+    "LK": "Sri Lanka",
+    "LR": "Liberia",
+    "LS": "Lesotho",
+    "LT": "Lithuania",
+    "LU": "Luxembourg",
+    "LV": "Latvia",
+    "LY": "Libya",
+
+    "MA": "Morocco",
+    "MC": "Monaco",
+    // "MD": "Republic of Moldova",
+    "MD": "Moldova",
+    "ME": "Montenegro",
+    // "MF": "Collectivity of Saint Martin",
+    "MF": "Saint Martin",
+    "MG": "Madagascar",
+    "MH": "Marshall Islands",
+    // "MK": "Republic of Macedonia",
+    "MK": "Macedonia",
+    "ML": "Mali",
+    "MM": "Myanmar",
+    "MN": "Mongolia",
+    "MO": "Macao",
+    "MP": "Northern Mariana Islands",
+    "MQ": "Martinique",
+    "MR": "Mauritania",
+    "MS": "Montserrat",
+    "MT": "Malta",
+    "MU": "Mauritius",
+    "MV": "Maldives",
+    "MW": "Malawi",
+    "MX": "Mexico",
+    "MY": "Malaysia",
+    "MZ": "Mozambique",
+
+    "NA": "Namibia",
+    "NC": "New Caledonia",
+    "NE": "Niger",
+    "NF": "Norfolk Island",
+    "NG": "Nigeria",
+    "NI": "Nicaragua",
+    "NL": "Netherlands",
+    "NO": "Norway",
+    "NP": "Nepal",
+    "NR": "Nauru",
+    "NU": "Niue",
+    "NZ": "New Zealand",
+
+    "OM": "Oman",
+
+    "PA": "Panama",
+    "PE": "Peru",
+    "PF": "French Polynesia",
+    "PG": "Papua New Guinea",
+    "PH": "Philippines",
+    "PK": "Pakistan",
+    "PL": "Poland",
+    "PM": "Saint Pierre and Miquelon",
+    "PN": "Pitcairn",
+    "PR": "Puerto Rico",
+    "PS": "Palestine, State of",
+    "PT": "Portugal",
+    "PW": "Palau",
+    "PY": "Paraguay",
+
+    "QA": "Qatar",
+
+    "RE": "Réunion",
+    "RO": "Romania",
+    "RS": "Serbia",
+    "RU": "Russian Federation",
+    "RW": "Rwanda",
+
+    "SA": "Saudi Arabia",
+    "SB": "Solomon Islands",
+    "SC": "Seychelles",
+    "SD": "Sudan",
+    "SE": "Sweden",
+    "SG": "Singapore",
+    // "SH": "Saint Helena, Ascension and Tristan da Cunha",
+    "SI": "Slovenia",
+    "SJ": "Svalbard and Jan Mayen",
+    "SK": "Slovakia",
+    "SL": "Sierra Leone",
+    "SM": "San Marino",
+    "SN": "Senegal",
+    "SO": "Somalia",
+    "SR": "Suriname",
+    "SS": "South Sudan",
+    "ST": "Sao Tome and Principe",
+    "SV": "El Salvador",
+    // "SX": "Sint Maarten",
+    "SY": "Syrian Arab Republic",
+    "SZ": "Swaziland",
+
+    "TC": "Turks and Caicos Islands",
+    "TD": "Chad",
+    // "TF": "French Southern Territories",
+    "TG": "Togo",
+    "TH": "Thailand",
+    "TJ": "Tajikistan",
+    "TK": "Tokelau",
+    "TL": "Timor-Leste",
+    "TM": "Turkmenistan",
+    "TN": "Tunisia",
+    "TO": "Tonga",
+    "TR": "Turkey",
+    "TT": "Trinidad and Tobago",
+    "TV": "Tuvalu",
+    "TW": "Taiwan",
+    // "TZ": "Tanzania",
+    "TZ": "United Republic of Tanzania",
+
+    "UA": "Ukraine",
+    "UG": "Uganda",
+    // "UM": "United States Minor Outlying Islands",
+    "US": "United States",
+    "UY": "Uruguay",
+    "UZ": "Uzbekistan",
+
+    "VA": "Vatican City",
+    "VC": "Saint Vincent and the Grenadines",
+    "VE": "Venezuela",
+    "VG": "British Virgin Islands",
+    "VI": "United States Virgin Islands",
+    "VN": "Vietnam",
+    "VU": "Vanuatu",
+
+    // "WF": "Wallis and Futuna",
+    "WS": "Samoa",
+
+    // "XK": "Republic of Kosovo",
+    "XK": "Kosovo",
+
+    "YE": "Yemen",
+    "YT": "Mayotte",
+
+    "ZA": "South Africa",
+    "ZM": "Zambia",
+    "ZW": "Zimbabwe",
+
+    "BRCL": "Brazil-Chile",
+    "ITNL": "Italy-Netherlands"
+  });
+  const FLAG_CODES = Object.freeze(Object.keys(FLAGS));
 
   /* help events */
   function $btnHelp_click() {
@@ -62,6 +334,17 @@
     $diaHelp.dataset.hidden = "";
   }
   $btnHelpClose.addEventListener("click", $btnHelpClose_click);
+
+  /* config events */
+  function $btnConfig_click() {
+    delete $diaConfig.dataset.hidden;
+  }
+  $btnConfig.addEventListener("click", $btnConfig_click);
+
+  function $btnConfigClose_click() {
+    $diaConfig.dataset.hidden = "";
+  }
+  $btnConfigClose.addEventListener("click", $btnConfigClose_click);
 
   /* example events */
   function $btnExample_click() {
@@ -105,34 +388,31 @@
   window.addEventListener("drop", window_drop);
 
   /* parsing events */
-  // TODO ParseFlags shouldn't be a class
-  class ParseFlags {
-    newKey(text) {
-      let key_text1 = text.match(/_..(?:..)?\]/)[0];
-      let key_text2 = key_text1.substring(1, (key_text1.length - 1));
-      return `[flag_${key_text2.toUpperCase()}]`;
-    }
+  function getCode(text) {
+    let code = text.match(/_..(?:..)?\]/)[0];
+    let codeFormatted = code.substring(1, (code.length - 1));
+    return codeFormatted.toUpperCase();
+  }
 
-    fixLink(text) {
-      let link2 = text.match(/\/..(?:..)?\./)[0];
-      let link3 = link2.substring(1, (link2.length - 1));
-      let ext = text.substring(text.lastIndexOf("."), text.length);
-      return `/flag/${link3.toUpperCase()}${ext}`;
-    }
+  function getNewKey(text) {
+    return `[flag_${getCode(text)}]`;
+  }
 
-    fixRef(text) {
-      let ref2 = text.match(/_..(?:..)?\]/)[0];
-      let ref3 = ref2.substring(1, (ref2.length - 1));
-      return `[flag_${ref3.toUpperCase()}]`;
-    }
+  function getFixedLink(text) {
+    let link2 = text.match(/\/..(?:..)?\./)[0];
+    let link3 = link2.substring(1, (link2.length - 1));
+    let ext = text.substring(text.lastIndexOf("."), text.length);
+    return `/flag/${link3.toUpperCase()}${ext}`;
+  }
+
+  function getFixedRef(text) {
+    return `[flag_${getCode(text)}]`;
   }
 
   $parse.addEventListener("click", () => {
     while ($diaErrorsList.firstChild) {
       $diaErrorsList.firstChild.remove();
     }
-
-    let patch = new ParseFlags();
 
     let lines = $input.value.split("\n");
     let flags_unsort = {};
@@ -151,10 +431,15 @@
             } else if (name2.length === 4) {
               ext = ".png";
             }
-            let newKey = key[j].replace(key[j], patch.newKey);
-            flags_unsort[newKey] = `/wiki/shared/flag/${name2}${ext}`;
-            if (!FLAGS.includes(name2)) {
+            let newKey = key[j].replace(key[j], getNewKey);
+            if (!FLAG_CODES.includes(name2)) {
               invalid_flags.push([name2, (i + 1)]);
+            }
+            // parse with broken flags anyways
+            if ($chkConfigCountryTitle.checked) {
+              flags_unsort[newKey] = `/wiki/shared/flag/${name2}${ext} "${FLAGS[name2] ? FLAGS[name2] : "NOT_FOUND"}"`;
+            } else {
+              flags_unsort[newKey] = `/wiki/shared/flag/${name2}${ext}`;
             }
           }
         }
@@ -162,18 +447,19 @@
       let link1 = lines[i].match(/\/flag\/..(?:..)?\.(gif|jpe?g|png)/g);
       if (link1) {
         for (let j = 0; j < link1.length; j++) {
-          lines[i] = lines[i].replace(link1[j], patch.fixLink);
+          lines[i] = lines[i].replace(link1[j], getFixedLink);
         }
       }
       let ref1 = lines[i].match(/\[flag_..(?:..)?\]/g);
       if (ref1) {
         for (let j = 0; j < ref1.length; j++) {
-          lines[i] = lines[i].replace(ref1[j], patch.fixRef);
+          lines[i] = lines[i].replace(ref1[j], getFixedRef);
         }
       }
     }
     if (invalid_flags.length > 0) {
       $btnErrors.textContent = `Errors (${invalid_flags.length})`;
+      $outputErrors.textContent = `${invalid_flags.length} error${invalid_flags.length === 1 ? "" : "s"} found`;
       for (let i = 0; i < invalid_flags.length; i++) {
         let $_li = document.createElement("li");
         $_li.textContent = `${invalid_flags[i][0]} (line: ${invalid_flags[i][1]})`;
@@ -181,21 +467,29 @@
       }
     } else {
       $btnErrors.textContent = "Errors (0)";
+      $outputErrors.textContent = "";
     }
-    lines = lines.join("\n");
-    let flags_sort = {};
-    Object.keys(flags_unsort).sort()
-      .forEach((key) => {
-        flags_sort[key] = flags_unsort[key];
-      });
-    for (let key in flags_sort) {
-      if (flags_sort.hasOwnProperty(key)) {
-        flags_output += `${key}: ${flags_sort[key]}\n`;
+
+    if (invalid_flags.length > 0 && !$chkConfigIgnoreErrors.checked) {
+      delete $diaErrors.dataset.hidden;
+    } else {
+      // we want the flags to go in some kind of order
+      let flags_sort = {};
+      Object.keys(flags_unsort).sort()
+        .forEach((key) => {
+          flags_sort[key] = flags_unsort[key];
+        });
+      for (let key in flags_sort) {
+        if (flags_sort.hasOwnProperty(key)) {
+          flags_output += `${key}: ${flags_sort[key]}\n`;
+        }
       }
+      if ($chkConfigOutputInput.checked) {
+        $output.textContent = `${lines.join("\n")}\n${flags_output}\n`;
+      } else {
+        $output.textContent = flags_output;
+      }
+      delete $diaOutput.dataset.hidden;
     }
-    // TEMP need to create a dialog to output results (just results)
-    // $input.value = lines + "\n" + flags_output;
-    $output.textContent = flags_output;
-    delete $diaOutput.dataset.hidden;
   });
 })();
