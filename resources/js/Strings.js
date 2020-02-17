9fabab4bd1f7ce231a1826ad984fb7c8628e4070
MegaApple_Pi <eric@m9pi.net>
2020-02-17 00:38:20 -0600

    Add French and Polish flag strings

--- resources/js/Strings.js
+++ resources/js/Strings.js
@@ -1,6 +1,10 @@
 import en from "./Strings/en.js";
+import fr from "./Strings/fr.js";
+import pl from "./Strings/pl.js";
 const Strings = {
     en,
+    fr,
+    pl,
 };
 export default Strings;
 //# sourceMappingURL=Strings.js.map
--- resources/ts/Strings.ts
+++ resources/ts/Strings.ts
@@ -1,7 +1,11 @@
 import en from "./Strings/en.js";
+import fr from "./Strings/fr.js";
+import pl from "./Strings/pl.js";
 
 const Strings: object = {
   en,
+  fr,
+  pl,
 };
 
 export default Strings;
