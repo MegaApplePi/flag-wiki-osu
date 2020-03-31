2b54c87a3ae62909f3bf6e9b77b2559b41c6fe37
MegaApple_Pi <eric@m9pi.net>
2020-03-31 15:09:54 -0500

    Add Chinese strings

--- resources/js/Strings.js
+++ resources/js/Strings.js
@@ -1,10 +1,12 @@
 import en from "./Strings/en.js";
 import fr from "./Strings/fr.js";
 import pl from "./Strings/pl.js";
+import zh from "./Strings/zh.js";
 const Strings = {
     en,
     fr,
     pl,
+    zh,
 };
 export default Strings;
 //# sourceMappingURL=Strings.js.map
--- resources/ts/Strings.ts
+++ resources/ts/Strings.ts
@@ -1,11 +1,13 @@
 import en from "./Strings/en.js";
 import fr from "./Strings/fr.js";
 import pl from "./Strings/pl.js";
+import zh from "./Strings/zh.js";
 
 const Strings: object = {
   en,
   fr,
   pl,
+  zh,
 };
 
 export default Strings;
