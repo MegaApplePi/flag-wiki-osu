d4083c709901a919c49c36599a674884bcd97562
MegaApple_Pi <eric@m9pi.net>
2020-05-17 15:49:39 -0500

    Add Turkish strings

--- resources/js/Strings.js
+++ resources/js/Strings.js
@@ -1,11 +1,13 @@
 import en from "./Strings/en.js";
 import fr from "./Strings/fr.js";
 import pl from "./Strings/pl.js";
+import tr from "./Strings/tr.js";
 import zh from "./Strings/zh.js";
 const Strings = {
     en,
     fr,
     pl,
+    tr,
     zh,
 };
 export default Strings;
--- resources/ts/Strings.ts
+++ resources/ts/Strings.ts
@@ -1,12 +1,14 @@
 import en from "./Strings/en.js";
 import fr from "./Strings/fr.js";
 import pl from "./Strings/pl.js";
+import tr from "./Strings/tr.js";
 import zh from "./Strings/zh.js";
 
 const Strings: object = {
   en,
   fr,
   pl,
+  tr,
   zh,
 };
 
